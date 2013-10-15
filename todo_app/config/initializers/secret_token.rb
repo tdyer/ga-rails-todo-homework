458b7bcfec7bdd0e7f0244800b06242f7ef157f5
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-10-15 18:20:17 -0400

    initialized rails in directory

---
+++ todo_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodoApp::Application.config.secret_key_base = 'ab3641db3e8e5fcdc58eb21334ec31211b23e877a52601278fde334d36b8cf263061b264a9557add50b0d7c9ec8e3f8784a030bb966e02d570d04c6109e85a5e'
