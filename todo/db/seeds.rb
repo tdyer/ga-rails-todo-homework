189ad4851c0f1d7b08e14d01cf759e0967b66479
Ben Winter <ben.g.winter@gmail.com>
2013-10-15 19:58:54 -0400

    ran migration, setting up seed data, setting up views

--- todo/db/seeds.rb
+++ todo/db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Todo.create(name: "The Joyous Dream", description: "Joyous", scary:false, fun:true)
+Todo.create(name: "The Angry Dream", description: "Angry", scary:true, fun:false)
+Todo.create(name: "The Evil Dream", description: "Evil", scary:true, fun:false)
