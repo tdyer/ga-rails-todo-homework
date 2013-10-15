a6b125c43dbb47566f339cee5968031b5a7b4f3a
Abby Howell <abby.g.howell@gmail.com>
2013-10-15 17:04:16 -0400

    adding create form

--- todo_app/app/controllers/tasks_controller.rb
+++ todo_app/app/controllers/tasks_controller.rb
@@ -5,12 +5,20 @@ class TasksController < ApplicationController
   end
 
   def index
+  	@tasks = Task.all
   end
 
   def new
+  	@task = Task.new
   end
 
   def create
+  	@task = Task.new(task_params)
+  	if @task.save
+  	  redirect_to @task
+  	else
+  	  render action: 'new'
+  	end
   end
 
   def edit
@@ -26,6 +34,10 @@ class TasksController < ApplicationController
 
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  def task_params
+  	params.require(:task).permit(:description, :flag, :due)
   end
 
 end
