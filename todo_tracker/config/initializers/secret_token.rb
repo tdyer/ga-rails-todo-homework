6ae8f3d43c3742c12074817f98ae6d6018a0eea2
Cori Allen <coriallen@gmail.com>
2013-10-16 09:26:17 -0400

    Try again, rails new todo_tracker.

---
+++ todo_tracker/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodoTracker::Application.config.secret_key_base = '2bde8d4d7367fb4875bb05532d3ae0ea729c2a233d4b146b7bb6147028474eecf720605fb2f538bfa786b91e6692254e6eea9c6bf69d8e02bfa5d2063972ac26'
---
+++ todo_tracker/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TodoTracker::Application.config.session_store :cookie_store, key: '_todo_tracker_session'
