4338a877b818e83fb3612e53d833bab134c04a8d
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-10-16 08:07:51 -0400

    added more erb files and defined create method in the item controller

--- todo_app/app/controllers/items_controller.rb
+++ todo_app/app/controllers/items_controller.rb
@@ -7,9 +7,13 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item= Item.new(item_params)
+    if @item.save
+      redirect_to items_url
   end
 
   def edit
