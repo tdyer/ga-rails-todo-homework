56a93d2af14cdd825f89521e9cd45fda3dca201b
Rahul Horé <hore.rahul@gmail.com>
2013-10-16 01:03:39 -0400

    CRUD done

--- todo_app/db/migrate/20131016012433_create_tasks.rb
+++ todo_app/db/migrate/20131016012433_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.text :description
-      t.boolean :complete?, default: false
+      t.boolean :complete?
       t.date :due_date
     end
   end
--- todo_app/db/schema.rb
+++ todo_app/db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131016012433) do
+ActiveRecord::Schema.define(version: 20131016032949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: true do |t|
     t.text    "description"
-    t.boolean "complete?", default: false
+    t.boolean "complete?",   default: false
     t.date    "due_date"
   end
 
