1526fa9dbb977fa0cd6a15d2b501859ca32e17ee
Ben Winter <ben.g.winter@gmail.com>
2013-10-15 20:59:35 -0400

    seed db

--- task_tracker/db/seeds.rb
+++ task_tracker/db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Task.create(description: "Walk The Dog", complete:false, due_date: "October 20, 2013")
+Task.create(description: "Wash The Dishes", complete:true, due_date: "October 25, 2013")
+Task.create(description: "Clean Kitchen", complete:true, due_date: "October 19, 2013")
